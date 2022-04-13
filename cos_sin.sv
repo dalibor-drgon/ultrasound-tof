f1fa830065b8d0380c612bd036dd693178301d2e
Dalibor Drgon <dalibor@drgon.me>
2022-04-13 14:35:19 +0200

    update cos_sin.sv

--- cos_sin.sv
+++ cos_sin.sv
@@ -1,15 +1,15 @@
 module cos_sin(
     input clk,
-    input [8:0] angle /* range 0 - 399 */, 
+    input [8:0] angle /* range 0 - 499 */, 
     output reg signed [47:0] cos,
     output reg signed [47:0] sin
 );
 
-    reg [47:0] cos_table [0:399];
-    initial $readmemh("cos400.hex", cos_table);
+    reg [47:0] cos_table [0:499];
+    initial $readmemh("cos500.hex", cos_table);
 
-    reg [47:0] sin_table [0:399];
-    initial $readmemh("sin400.hex", sin_table);
+    reg [47:0] sin_table [0:499];
+    initial $readmemh("sin500.hex", sin_table);
     
     always @(posedge clk) begin 
         cos <= cos_table[angle];
